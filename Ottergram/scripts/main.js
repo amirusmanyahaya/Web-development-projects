309e84f0bd585105c02abfd92270fca886beb424
Yahaya Usman <yahus591@student.liu.se>
2021-02-03 02:07:32 +0100

    added a toggle to the detail image

--- Ottergram/scripts/main.js
+++ Ottergram/scripts/main.js
@@ -1,7 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-title="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-
+var HIDDEN_DETAILS_CLASS = "hidden-details";
+var ESC_KEY = "Escape";
 function setDetails(imageUrl, titleText) {
   "use strict";
   var detailedImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -30,18 +31,42 @@ function addThumbClickHandler(thumbnail) {
   thumbnail.addEventListener("click", function (event) {
     event.preventDefault();
     setDetailsFromThumb(thumbnail);
+    showDetails(HIDDEN_DETAILS_CLASS);
   });
 }
 
 function getAllThumbnails() {
+  "use strict";
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailsArray = [].slice.call(thumbnails);
   return thumbnailsArray;
 }
 
+function hideDetails(className) {
+  "use strict";
+  document.body.classList.add(className);
+}
+
+function showDetails(className) {
+  "use strict";
+  document.body.classList.remove(className);
+}
+
+function addKeyHandler() {
+  "use strict";
+  document.body.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.code === ESC_KEY) {
+      hideDetails(HIDDEN_DETAILS_CLASS);
+    }
+  });
+}
+
 function initialize() {
+  "use strict";
   var thumbnails = getAllThumbnails();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyHandler();
 }
 
 initialize();
