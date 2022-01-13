5fdc84a148ec1d75885d2f903a412442653fe2f5
Yahaya Usman <amirusman20@gmail.com>
2022-01-13 01:52:05 +0100

    create new interface

--- Calculator/scripts/main.js
+++ Calculator/scripts/main.js
@@ -1,11 +1,23 @@
 const RESULT_DISPLAY = document.querySelector('.result-display')
 const CALC_BUTTONS = document.querySelectorAll('.btn-calc')
+
+let firstValue = ""
+let secondValue = ""
+
+
 const displayValue = (value) => {
     if(value === "CLS")
         RESULT_DISPLAY.textContent = ""
     else
         RESULT_DISPLAY.textContent = `${RESULT_DISPLAY.textContent}${value}`
 
+}
+
+const validateValue = (value) => {
+    const displayContent = RESULT_DISPLAY.textContent
+    switch(value){
+        case '1':
+    }
 }
 
 const calcButtons = Array.from(CALC_BUTTONS)
